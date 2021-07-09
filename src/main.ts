67f6e34851c5c02eaf3d3d08a3d648237648741a
Daniel <daniel_cornock@hotmail.co.uk>
2021-07-09 08:28:56 +0100

    task(security): add base security headers

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as rateLimit from 'express-rate-limit';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 import { ConfigService } from './core/services/config/config.service';
 
@@ -8,8 +10,13 @@ async function bootstrap(): Promise<void> {
 
   app.setGlobalPrefix('api/v1');
   app.enableCors({
-    origin: ['http://localhost:4500']
+    origin: ['http://localhost:4500', 'https://https://recovqr.netlify.app']
   });
+  app.use(helmet());
+  app.use(rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 150
+  }));
   
   const port = ConfigService.getPort();
   await app.listen(port);
